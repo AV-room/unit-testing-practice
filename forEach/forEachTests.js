25eb4246af47c9c065f9d43bbf77fbe608f7aa63
AV-room <AVroom.github@gmail.com>
2017-12-29 13:37:20 +1030

    added examples to base tests on

--- forEach/forEachTests.js
+++ forEach/forEachTests.js
@@ -1,3 +1,37 @@
 //using a test-driven development approach to write a forEach() function from scratch
 //tests are written in vanilla JS
 
+//native JS forEach()
+var myArray = [1,2,3];
+myArray.forEach(function(current){
+   console.log(current*2);
+}) //2 4 6
+
+console.log(myArray); //[1,2,3]
+
+//EXAMPLES 
+//i.e. how we'd like our from-scratch forEach() to work
+/*forEach([1,2,3], function(){
+    console.log('HEY!');
+})*/ 
+// HEY | HEY | HEY
+
+/*forEach([1,2,3], function(element){
+    console.log(element);
+})*/ 
+// 1 | 2 | 3
+
+/*forEach([1,2,3], function(element, index){
+    console.log('[' + index + ']= ' + element + ' ');
+})*/ 
+// [0]= 1 | [1]= 2 | [2]= 3
+
+/*forEach([1,2,3], function(element, index, array){
+    console.log('[' + index + '`]= ' + array[index] + ' ');
+}) */
+// [0`]= 1 | [1`]= 2 | [2`]= 3
+
+
+
+
+
