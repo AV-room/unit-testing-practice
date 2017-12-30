93b840f6d937174c82df568a9cefad74805d1830
AV-room <AVroom.github@gmail.com>
2017-12-30 22:45:18 +1030

    added scaffolding for filter() work

---
+++ filter/myFilterFunc.js
@@ -0,0 +1,5 @@
+//using a test-driven development approach to write a filter() function from scratch
+//tests are written using tinytest.js (https://github.com/joewalnes/jstinytest)
+
+/*--- CODE TO TEST---*/
+console.log('greetings from inside myFilterFunc.js');
