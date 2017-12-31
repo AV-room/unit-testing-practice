3167e04df0d6e35a8cfbb613a1606047fd7a83fd
AV-room <AVroom.github@gmail.com>
2017-12-31 15:28:32 +1030

    all tests passing

--- filter/myFilterFunc.js
+++ filter/myFilterFunc.js
@@ -2,8 +2,20 @@
 //tests are written using tinytest.js (https://github.com/joewalnes/jstinytest)
 
 /*--- CODE TO TEST---*/
-function filter(array, callbackFunction) {
-    for(var i=0; i<array.length; i++) {
-        callbackFunction(array[i], i, array);
+function filter(array, callbackFunction, optionalThisObj) {
+    
+    var result = [];
+    var filterCallbackFunction = callbackFunction;
+    
+    if(optionalThisObj) {
+        filterCallbackFunction = callbackFunction.bind(optionalThisObj);
     }
+    
+    for(var i=0; i<array.length; i++) {
+        if(filterCallbackFunction(array[i], i, array)) {
+           result.push(array[i]);
+        }
+    }
+    
+    return result;
 }
