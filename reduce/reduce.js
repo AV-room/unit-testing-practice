048bb39fc4b45ec992d0cfdc6bc09b20e354119b
AV-room <AVroom.github@gmail.com>
2018-01-05 14:18:44 +1030

    summary of tests

--- reduce/reduce.js
+++ reduce/reduce.js
@@ -1,5 +1,11 @@
-function reduce() {
-    return 'inside reduce()';
+function reduce(array, callback, startingValue) {
+    var resultSoFar = startingValue;
+    
+    for(let i=0; i<array.length; i++) {
+        resultSoFar = callback(resultSoFar, array[i]);
+    }
+    
+    return resultSoFar;
 }
 
 module.exports = reduce;
--- reduce/reduce.test.js
+++ reduce/reduce.test.js
@@ -3,3 +3,18 @@ const reduce = require('./reduce');
 test('simple test', () => {
    expect(reduce()).toBe('inside reduce()'); 
 });
+
+//test callback args (accumulator, currentVal, currentIndex, array) 
+
+//test callback called for each assigned index in array
+//test reduced value returned (happy path)
+
+//test initialValue arg present => accumulator = initialVal, currentVal = array[0], callback starts at index 0
+
+//test initialValue arg not present => accumulator = array[0], currentVal = array[1], callback starts at index 1
+
+//EDGE CASES
+//test holes skipped
+//array empty, no initialValue arg present => TypeError thrown
+//array empty, initialValue arg present => return initialValue without calling callback
+//array has one element, no initialValue arg present => return solo element without calling callback
